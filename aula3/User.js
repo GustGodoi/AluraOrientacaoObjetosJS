a5494a16f304d4604848e60aa44e6fd2aeb3b510
GustGodoi <gustavogodoi442@gmail.com>
2023-11-07 21:52:06 -0300

    Aprendendo sobre encapsulamento em JS, sobre métodos Getters e Setters.

--- aula3/User.js
+++ aula3/User.js
@@ -3,7 +3,7 @@
 export default class User {
     // as '#' fazem com que o atributo da classe seja restrito,
     // podendo ser alterado somente por dentro da própria classe
-    
+
     #nome
     #email
     #nascimento
@@ -17,7 +17,61 @@ export default class User {
         this.#ativo = ativo
     }
 
+    get nome() {
+        return this.#nome
+    }
+
+    get email() {
+        return this.#email
+    }
+
+    get nascimento() {
+        return this.#nascimento
+    }
+
+    get role() {
+        return this.#role
+    }
+
+    get ativo() {
+        return this.#ativo
+    }
+
+    set nome(novoNome) {
+        if (novoNome === '') {
+            throw new Error('formato não esperado.')
+        }
+        this.#nome = novoNome
+    }
+
+    set email(novoEmail) {
+        this.#email = novoEmail
+    }
+
+    set nascimento(novoNascimento) {
+        this.#nascimento = novoNascimento
+    }
+
+    set role(novaRole) {
+        this.#role = novaRole
+    }
+
+    set ativo(novoAtivo) {
+        this.#ativo = novoAtivo
+    }
+
+    // função restrita de exemplo, criada junto de '#'
+    // #montaObjUser() {
+    //     return ({
+    //         nome: this.#nome,
+    //         email: this.#email,
+    //         nascimento: this.#nascimento,
+    //         role: this.#role,
+    //         ativo: this.#ativo
+    //     })
+    // }
+
     exibirInfos() {
-        return `${this.#nome}, ${this.#email}`
+        return `${this.nome}, ${this.email}`
     }
 }
--- index.js
+++ index.js
@@ -2,8 +2,12 @@ import User from './aula3/User.js';
 import Admin from './aula3/Admin.js';
 import Docente from './aula3/Docente.js';
 
-const novoUser = new User('Mariana', 'm@m.com', '2021-01-01')
-console.log(novoUser.exibirInfos())
+const novoUser = new User('Mariana', 'm@m.com', '2021-01-01');
+// console.log(novoUser.exibirInfos());
 
-// novoUser.#nome = 'Marcia'
-// console.log(novoUser.exibirInfos())
+const novoAdmin = new Admin('Rodrigo','R@R.com','2020-01-01');
+// console.log(novoAdmin.exibirInfos());
+console.log(novoAdmin.nome);
+novoAdmin.nome = 'Andre'
+console.log(novoAdmin.nome);
+console.log(novoAdmin.exibirInfos());
